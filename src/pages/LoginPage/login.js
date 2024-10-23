5fa0591f554feaa1e3a715cb27df9cb6fe716843
ohgoodgood <jskim9504@gmail.com>
2024-10-23 09:26:07 +0900

    feature: 로그인 페이지 기능 구현, API 연동 (2차)

--- src/pages/LoginPage/login.js
+++ src/pages/LoginPage/login.js
@@ -5,6 +5,26 @@ document.addEventListener('DOMContentLoaded', function () {
   const loginButton = document.querySelector('.login-btn');
   const signupButton = document.querySelector('.signup-btn');
 
+  // 페이지 로드 시, 캐시 무시하고 강제 새로고침
+  window.addEventListener('pageshow', function (event) {
+    if (event.persisted) {
+      this.window.location.reload();
+    }
+  });
+
+  // 페이지 로드 시 인풋 필드 초기화
+  emailInput.value = '';
+  passwordInput.value = '';
+  rememberLogin.checked = false;
+
+  // 다른 페이지에서 로그인 페이지로 진입한 경우, 직전 URL을 저장 (이미 저장된 값이 없는 경우에만)
+  if (
+    !sessionStorage.getItem('prevPage') &&
+    document.referrer !== window.location.href
+  ) {
+    sessionStorage.setItem('prevPage', document.referrer);
+  }
+
   // 이메일 주소 유효성 검사
   function validateEmail(email) {
     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -36,13 +56,15 @@ document.addEventListener('DOMContentLoaded', function () {
     const email = emailInput.value;
     const password = passwordInput.value;
     const remember = rememberLogin.checked;
-    const apiUrl = import.meta.env.VITE_API_HOST; // 환경 변수 차용
+    // 환경 변수 사용
+    const apiUrl = import.meta.env.VITE_API_URL;
+    const clientId = import.meta.env.VITE_CLIENT_ID;
 
     // API 연결
     try {
       const response = await fetch(`${apiUrl}/users/login`, {
         method: `POST`,
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 'Content-Type': 'application/json', 'client-id': clientId },
         body: JSON.stringify({
           email: email,
           password: password,
@@ -52,20 +74,28 @@ document.addEventListener('DOMContentLoaded', function () {
       // 로그인 성공
       if (response.ok) {
         const data = await response.json();
-        window.location.href =
-          'http://192.168.45.139:5173/src/pages/MainPage/index.html'; // 메인화면으로 이동
+        console.log(data); // 응답 데이터가 잘 왔는지 확인
 
+        // 로그인 상태 저장
         if (remember) {
-          // '로그인 정보 저장' 체크 시 localStorage에 저장
-          localStorage.setItem(`accessToken`, data.token.accessToken);
-          localStorage.setItem(`refreshToken`, data.token.refreshToken);
+          localStorage.setItem(`accessToken`, data.item.token.accessToken);
+          localStorage.setItem(`refreshToken`, data.item.token.refreshToken);
           localStorage.setItem(`email`, email);
+          localStorage.setItem(`password`, password);
         } else {
-          // '로그인 정보 저장' 체크 해제 시 sessionStorage에 저장
-          sessionStorage.setItem(`accessToken`, data.token.accessToken);
-          sessionStorage.setItem(`refreshToken`, data.token.refreshToken);
+          sessionStorage.setItem(`accessToken`, data.item.token.accessToken);
+          sessionStorage.setItem(`refreshToken`, data.item.token.refreshToken);
           sessionStorage.setItem(`email`, email);
         }
+
+        // 이전 페이지로 리다이렉트 (저장된 URL로 이동, 기본값은 메인 페이지 url)
+        const redirectUrl =
+          sessionStorage.getItem('prevPage') ||
+          'http://192.168.45.139:5173/src/pages/MainPage/index.html';
+        window.location.href = redirectUrl;
+
+        // URL 삭제 (한 번 리다이렉트 후, 다음에는 적용되지 않도록)
+        sessionStorage.removeItem('prevPage');
 
         // 로그인 실패
       } else {
@@ -73,7 +103,7 @@ document.addEventListener('DOMContentLoaded', function () {
         alert('로그인 실패: ' + errorData.message);
       }
 
-      // 오류 발생
+      // 오류 발생 시
     } catch (error) {
       console.log('로그인 중 오류 발생: ', error);
       alert(`로그인 중 오류가 발생했습니다.`);
@@ -86,14 +116,15 @@ document.addEventListener('DOMContentLoaded', function () {
       'http://192.168.45.139:5173/src/pages/signupPage/index.html';
   });
 
-  // 페이지 로드 시 바로 form 유효성 검사 --> 로그인 버튼 비활성화 상태로 초기화
-  validateForm();
-
-  // 페이지 로드 시 저장된 이메일 값이 있으면 불러오기
-  const savedEmail =
-    localStorage.getItem('email') || sessionStorage.getItem('email');
-  if (savedEmail) {
-    emailInput.value = savedEmail;
-    rememberLogin.checked = !!localStorage.getItem('email');
+  // 로그인 정보 저장 여부에 따라 인풋 필드 설정
+  if (localStorage.getItem('email') && localStorage.getItem('password')) {
+    emailInput.value = localStorage.getItem('email');
+    passwordInput.value = localStorage.getItem('password');
+  } else {
+    emailInput.value = '';
+    passwordInput.value = '';
   }
+
+  // form 유효성 검사 --> 로그인 버튼 비활성화 상태로 초기화
+  validateForm();
 });
