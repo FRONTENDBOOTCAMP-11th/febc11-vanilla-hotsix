9ba2656f552de0d9c45499c340270adbd1bd0828
Youjiiin <yujinjang24@gmail.com>
2024-10-23 13:10:03 +0900

    feature: 작가 홈 통신(유저는 static)

--- src/pages/AuthorPage/author.js
+++ src/pages/AuthorPage/author.js
@@ -1,32 +1,36 @@
 'use strict';
 
 import axios from 'axios';
+
+// 이미지 렌더링 API
 import getImg from '../../api/getImg';
 
 // 환경 변수 가져오기
 const apiUrl = import.meta.env.VITE_API_URL;
 const clientId = import.meta.env.VITE_CLIENT_ID;
-console.log(clientId)
 
 // 받아올 유저정보
 const userName = document.querySelector('.author-info__name');
 const userRole = document.querySelector('.author-info__role');
 const userImg = document.querySelector('.author-info__avatar');
+const bookmarkedBy = document.querySelector('.bookmarkedBy'); // 구독자
+const bookmarked = document.querySelector('.bookmarked'); // 관심작가
 
 // 유저 정보 받아오기 통신
 const getUserInfo = async () => {
   try {
-    const res = await axios.get(apiUrl + '/users/1', {
+    const res = await axios.get(apiUrl + `/users/2`, {
       headers: {
         'client-id': clientId,
+        'Content-Type': 'application/json',
       },
     });
 
-    console.log(res.data);
-
     // 유저정보 넣기
     userName.innerHTML = res.data.item.name;
-    userRole.innerHTML = res.data.item.type;
+    userRole.innerHTML = res.data.item.extra.job;
+    bookmarkedBy.innerHTML = res.data.item.bookmarkedBy.users;
+    bookmarked.innerHTML = res.data.item.bookmark.users;
 
     // 이미지 src 가져오기 (비동기 처리 대기)
     const ImgSrc = await getImg(res.data.item.image);
@@ -56,33 +60,34 @@ const monthNames = [
   'Dec',
 ];
 
+// 해당 유저의 게시물 목록 가져오기
 const getUserPost = async () => {
   try {
-    const res = await axios.get(apiUrl + `/posts/users/1?type=info`, {
+    const res = await axios.get(apiUrl + `/posts/users/2?type=info`, {
       headers: {
         'client-id': clientId,
+        'Content-Type': 'application/json',
       },
     });
 
-    console.log(res.data.item);
-
     // 게시글 렌더링 하기
+    // 게시글이 없으면 undefined (옵셔널 체이닝)
     const posts = res.data?.item;
-    console.log(posts);
+
     if (posts) {
       postList.innerHTML = posts
         .map(post => {
+          // 날짜 형식 변경하기 (2024.01.01 -> Jan 01. 2024)
           const [datePart, timePart] = post.updatedAt.split(' ');
           const [year, month, day] = datePart.split('.');
           const dateObj = new Date(`${year}-${month}-${day}`);
-
           const formattedDate = `${monthNames[dateObj.getMonth()]} ${day}. ${year}`;
 
           return `
           <li class="post">
             <p class="post-category">${post.type}</p>
             <h3 class="post-title">${post.title}</h3>
-            <p class="post-content">${post.content}</p>
+            <span class="post-content">${post.content}</span>
             <div class="post-status-container">
               <p class="post-status">댓글${post.repliesCount}</p>
               <p class="post-status">${formattedDate}</p>
@@ -91,8 +96,21 @@ const getUserPost = async () => {
         `;
         })
         .join('');
+
+      // HTML로 오는 게시글 텍스트로 전환
+      const postContents = document.querySelectorAll('.post-content');
+      postContents.forEach((contentElement, index) => {
+        // 태그 사이의 텍스트만 추출
+        const textOnly = contentElement.textContent.trim();
+
+        // post.extra.subTitle 가져오기
+        const subTitle = posts[index].extra.subTitle;
+
+        // 텍스트만 추출한 후 다시 post-content에 삽입
+        contentElement.textContent = `${subTitle} | ${textOnly}`;
+      });
     } else {
-      console.error('Posts data is unavailable or not in the expected format');
+      console.error('게시글을 불러오는 데 실패했습니다.');
       postList.innerHTML = '<p>게시글을 불러오는 데 실패했습니다.</p>';
     }
   } catch (err) {
