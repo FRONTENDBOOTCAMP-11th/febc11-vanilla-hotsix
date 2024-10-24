950143e69103566f5cc4ed082ee0489e445b77f3
Youjiiin <yujinjang24@gmail.com>
2024-10-24 13:26:10 +0900

    fix: 배포 관련 설정 변경

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: './',
   build: {
+    target: 'esnext',
     rollupOptions: {
       input: {
         main: 'src/pages/MainPage/index.html',
